0cb163f7a89503d5e7e3eecaffa31dbfbf60dd95
Kamil Wisniewski <kamil.m.wis@gmail.com>
2021-06-29 14:43:56 +0200

    V 0.1.2 - fixed problem with CrossOrigin

--- src/main/java/org/gamepals/gamepalsapi/announcement/AnnouncementController.java
+++ src/main/java/org/gamepals/gamepalsapi/announcement/AnnouncementController.java
@@ -2,6 +2,7 @@ package org.gamepals.gamepalsapi.announcement;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/announcement")
+@CrossOrigin
 public class AnnouncementController {
     public AnnouncementController(AnnouncementService service) {
         this.service = service;
