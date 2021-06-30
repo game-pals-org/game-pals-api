0ba80be7120936c62f8a1f50d8106c61be542467
Kamil Wisniewski <kamil.m.wis@gmail.com>
2021-06-30 12:14:32 +0200

    V 0.3.1 - fixed no serializer in Announcement

--- src/main/java/org/gamepals/gamepalsapi/announcement/Announcement.java
+++ src/main/java/org/gamepals/gamepalsapi/announcement/Announcement.java
@@ -8,11 +8,50 @@ public class Announcement {
     String discordName;
     String additionalInfo;
 
-    public Announcement( Long id, String nick, String gameName, String discordName, String additionalInfo) {
+    public Announcement() {
+    }
+
+    public Announcement(Long id, String nick, String gameName, String discordName, String additionalInfo) {
         this.id = id;
         this.nick = nick;
         this.gameName = gameName;
         this.discordName = discordName;
+        this.additionalInfo = additionalInfo;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getNick() {
+        return nick;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+
+    public String getGameName() {
+        return gameName;
+    }
+
+    public void setGameName(String gameName) {
+        this.gameName = gameName;
+    }
+
+    public String getDiscordName() {
+        return discordName;
+    }
+
+    public void setDiscordName(String discordName) {
+        this.discordName = discordName;
+    }
+
+    public String getAdditionalInfo() {
+        return additionalInfo;
+    }
+
+    public void setAdditionalInfo(String additionalInfo) {
         this.additionalInfo = additionalInfo;
     }
 
