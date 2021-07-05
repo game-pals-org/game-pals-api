800171af5468a14db744116ed34d08467ccebab4
Janek <jaaaanek99@gmail.com>
2021-07-05 11:54:52 +0200

    Added Delete Method To Controller

--- src/main/java/org/gamepals/gamepalsapi/announcement/AnnouncementController.java
+++ src/main/java/org/gamepals/gamepalsapi/announcement/AnnouncementController.java
@@ -20,48 +20,59 @@ public class AnnouncementController {
     }
 
     @GetMapping
-    public List<Announcement> getAllAnnouncements(){
+    public List<Announcement> getAllAnnouncements() {
         return announcementService.getAnnouncements();
     }
 
     @PostMapping
-    public Announcement addAnnouncement(@RequestBody Announcement announcement){
+    public Announcement addAnnouncement(@RequestBody Announcement announcement) {
         return announcementService.addAnnouncement(announcement);
     }
 
-    @GetMapping("/gamenameasc")
-    public List<Announcement> getAnnouncementsByGameNameAsc(){
-        return announcementService.getAnnouncementsByGameNameAsc();
-    }
-    @GetMapping("/gamenamedesc")
-    public List<Announcement> getAnnouncementsByGameNameDesc(){
-        return announcementService.getAnnouncementsByGameNameDesc();
-    }
-    @GetMapping("/dateasc")
-    public List<Announcement> getAnnouncementsByDateAsc(){
-        return announcementService.getAnnouncementsByDateAsc();
-    }
-    @GetMapping("/datedesc")
-    public List<Announcement> getAnnouncementsBtDateDesc(){
-        return announcementService.getAnnouncementsBtDateDesc();
-    }
-    @GetMapping("/ranked")
-    public List<Announcement> getAnnouncementsForRanked(){
-        return announcementService.getAnnouncementsForRanked();
-    }
-    @GetMapping("/casual")
-    public List<Announcement> getAnnouncementsForCasual(){
-        return announcementService.getAnnouncementsForCasual();
-    }
-    @GetMapping("/{name}")
-    public List<Announcement> getAnnouncementsBySearchedGame(@PathVariable String name){
-        return announcementService.getAnnouncementsBySearchedGame(name);
-    }
-    @GetMapping("/byuser/{username}")
-    public List<Announcement> getAnnouncementsFromUserWithUsername(@PathVariable String username){
-        return announcementService.getAnnouncementsFromUserWithUsername(username);
+    @DeleteMapping
+    public Announcement deleteAnnouncement(Long id) {
+        return announcementService.deleteAnnouncementById(id);
     }
 
+    @GetMapping("/gamenameasc")
+    public List<Announcement> getAnnouncementsByGameNameAsc() {
+        return announcementService.getAnnouncementsByGameNameAsc();
+    }
+
+    @GetMapping("/gamenamedesc")
+    public List<Announcement> getAnnouncementsByGameNameDesc() {
+        return announcementService.getAnnouncementsByGameNameDesc();
+    }
+
+    @GetMapping("/dateasc")
+    public List<Announcement> getAnnouncementsByDateAsc() {
+        return announcementService.getAnnouncementsByDateAsc();
+    }
+
+    @GetMapping("/datedesc")
+    public List<Announcement> getAnnouncementsBtDateDesc() {
+        return announcementService.getAnnouncementsBtDateDesc();
+    }
+
+    @GetMapping("/ranked")
+    public List<Announcement> getAnnouncementsForRanked() {
+        return announcementService.getAnnouncementsForRanked();
+    }
+
+    @GetMapping("/casual")
+    public List<Announcement> getAnnouncementsForCasual() {
+        return announcementService.getAnnouncementsForCasual();
+    }
+
+    @GetMapping("/{name}")
+    public List<Announcement> getAnnouncementsBySearchedGame(@PathVariable String name) {
+        return announcementService.getAnnouncementsBySearchedGame(name);
+    }
+
+    @GetMapping("/byuser/{username}")
+    public List<Announcement> getAnnouncementsFromUserWithUsername(@PathVariable String username) {
+        return announcementService.getAnnouncementsFromUserWithUsername(username);
+    }
 
 
 }
